4d27f5e21ed73ee78259ecb1b70832e562785d31
sandeep-rana-19 <96654527+sandeeprana191@users.noreply.github.com>
2024-09-28 17:10:16 +0530

    ep-15 completed

--- ep-15-aynchronous-javascript-and-event-loop-from-scratch.js
+++ ep-15-aynchronous-javascript-and-event-loop-from-scratch.js
@@ -1,2 +1,37 @@
 // EP-15 | Asynchronous JavaScript & EVENT LOOP from scratch 🔥
+/*
 // USE CASE 1:
+console.log("Start");
+
+setTimeout(function cb() {
+  console.log("Callback");
+}, 5000);
+
+console.log("End");
+*/
+
+/*
+// USE CASE 2:
+console.log("Start");
+
+document.getElementById("btn").addEventListener('click',function cb(){
+  console.log("Callback")
+})
+
+console.log("End");
+*/
+
+/*
+// USE CASE 3:
+console.log("Start");
+
+setTimeout(function cbT() {
+  console.log("CB SetTimeout");
+}, 5000);
+
+fetch("http://api.netflix.com").then(function cbF() {
+  console.log("CB Netflix");
+});
+
+console.log("End");
+*/
