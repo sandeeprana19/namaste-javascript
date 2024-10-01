31c683dd1cbef6bdd5b0356df1e11aeea2886222
Sandeep Rana <sandeep.rana@capitalnumbers.com>
2024-10-01 15:40:06 +0530

    season-2 ep-1 accomplished

---
+++ season-2/ep-01-callback-hell.js
@@ -0,0 +1,19 @@
+// EP-01 | Callback Hell
+
+/*
+// USE CASE 1:
+console.log("Namaste");
+
+console.log("JavaScript");
+
+console.log("Season 2");
+*/
+
+// USE CASE 1:
+console.log("Namaste");
+
+setTimeout(function () {
+  console.log("JavaScript");
+}, 5000);
+
+console.log("Season 2");
