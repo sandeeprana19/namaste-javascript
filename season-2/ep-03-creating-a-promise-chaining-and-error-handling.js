98b7af09175d43e4c4aa5740a2c5e0622ac317a0
sandeep-rana-19 <96654527+sandeeprana191@users.noreply.github.com>
2024-10-04 09:11:19 +0530

    season-02 ep-03 lip

--- season-2/ep-03-creating-a-promise-chaining-and-error-handling.js
+++ season-2/ep-03-creating-a-promise-chaining-and-error-handling.js
@@ -1,3 +1,45 @@
 // EP-03 | Creating a Promise, Chaining & Error Handling
 
 // USE CASE 1:
+const cart = ["shoe", "pant", "kurta"];
+
+const promise = createOrder(cart); // orderId
+
+console.log(promise);
+
+// Consumer
+promise
+  .then(function (orderId) {
+    console.log(orderId);
+    // proceedToPayment(orderId);
+  })
+  .catch(function (err) {
+    console.log(err.message);
+  });
+
+// Producer
+function createOrder(cart) {
+  const pr = new Promise(function (resolve, reject) {
+    // createOrder
+    // validateCart
+    // orderId
+    if (!validateCart(cart)) {
+      const err = new Error("Cart is not valid");
+      reject(err);
+    }
+
+    const orderId = "12345";
+
+    if (orderId) {
+      setTimeout(() => {
+        resolve(orderId);
+      }, 5000);
+    }
+  });
+
+  return pr;
+}
+
+function validateCart(cart) {
+  return false;
+}
