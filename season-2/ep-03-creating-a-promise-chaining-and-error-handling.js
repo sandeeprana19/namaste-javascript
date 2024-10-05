4fa3b1b2607465b5b067da242dc90f65a2c6ae1f
sandeep-rana-19 <96654527+sandeeprana191@users.noreply.github.com>
2024-10-05 17:24:26 +0530

    season-02 ep-03 accomplished

--- season-2/ep-03-creating-a-promise-chaining-and-error-handling.js
+++ season-2/ep-03-creating-a-promise-chaining-and-error-handling.js
@@ -1,5 +1,6 @@
 // EP-03 | Creating a Promise, Chaining & Error Handling
 
+/*
 // USE CASE 1:
 const cart = ["shoe", "pant", "kurta"];
 
@@ -43,3 +44,243 @@ function createOrder(cart) {
 function validateCart(cart) {
   return false;
 }
+*/
+
+/*
+// USE CASE 2:
+const cart = ["shoe", "pant", "kurta"];
+
+createOrder(cart)
+  .then(function (orderId) {
+    console.log(orderId);
+    return orderId;
+  })
+  .then(function (orderId) {
+    return proceedToPayment(orderId);
+  })
+  .then(function (paymentInfo) {
+    console.log(paymentInfo);
+  })
+  .catch(function (err) {
+    console.log(err.message);
+  });
+
+// Producer
+function createOrder(cart) {
+  const pr = new Promise(function (resolve, reject) {
+    // createOrder
+    // validateCart
+    // orderId
+    if (!validateCart(cart)) {
+      const err = new Error("Cart is not valid");
+      reject(err);
+    }
+
+    const orderId = "12345";
+
+    if (orderId) {
+      setTimeout(() => {
+        resolve(orderId);
+      }, 5000);
+    }
+  });
+
+  return pr;
+}
+
+function validateCart(cart) {
+  return true;
+}
+
+function proceedToPayment(orderId) {
+  return new Promise(function (resolve, reject) {
+    resolve("Payment successful");
+  });
+}
+*/
+
+/*
+// USE CASE 3:
+const cart = ["shoe", "pant", "kurta"];
+
+createOrder(cart)
+  .then(function (orderId) {
+    console.log(orderId);
+    return orderId;
+  })
+  .catch(function (err) {
+    console.log(err.message);
+  })
+  .then(function (orderId) {
+    return proceedToPayment(orderId);
+  })
+  .then(function (paymentInfo) {
+    console.log(paymentInfo);
+  });
+
+// Producer
+function createOrder(cart) {
+  const pr = new Promise(function (resolve, reject) {
+    // createOrder
+    // validateCart
+    // orderId
+    if (!validateCart(cart)) {
+      const err = new Error("Cart is not valid");
+      reject(err);
+    }
+
+    const orderId = "12345";
+
+    if (orderId) {
+      setTimeout(() => {
+        resolve(orderId);
+      }, 5000);
+    }
+  });
+
+  return pr;
+}
+
+function validateCart(cart) {
+  return false;
+}
+
+function proceedToPayment(orderId) {
+  return new Promise(function (resolve, reject) {
+    resolve("Payment successful");
+  });
+}
+*/
+
+/*
+// USE CASE 4:
+const cart = ["shoe", "pant", "kurta"];
+
+createOrder(cart)
+  .then(function (orderId) {
+    console.log(orderId);
+    return orderId;
+  })
+  .catch(function (err) {
+    console.log(err.message);
+  })
+  .then(function (orderId) {
+    return proceedToPayment(orderId);
+  })
+  .then(function (paymentInfo) {
+    console.log(paymentInfo);
+  })
+  .catch(function (err) {
+    console.log(err.message);
+  })
+  .then(function () {
+    console.log("No matter what happens, I will definately be called.");
+  });
+
+// Producer
+function createOrder(cart) {
+  const pr = new Promise(function (resolve, reject) {
+    // createOrder
+    // validateCart
+    // orderId
+    if (!validateCart(cart)) {
+      const err = new Error("Cart is not valid");
+      reject(err);
+    }
+
+    const orderId = "12345";
+
+    if (orderId) {
+      setTimeout(() => {
+        resolve(orderId);
+      }, 5000);
+    }
+  });
+
+  return pr;
+}
+
+function validateCart(cart) {
+  return false;
+}
+
+function proceedToPayment(orderId) {
+  return new Promise(function (resolve, reject) {
+    resolve("Payment successful");
+  });
+}
+*/
+
+/*
+// USE CASE 5:
+// createOrder,
+// proceedToPayment,
+// showOrderSummary,
+// updateWalletBalance
+
+const cart = ["shoe", "pant", "kurta"];
+
+createOrder(cart)
+  .then(function (orderId) {
+    console.log(orderId);
+    return orderId;
+  })
+  .then(function (orderId) {
+    return proceedToPayment(orderId);
+  })
+  .then(function (paymentInfo) {
+    console.log(paymentInfo);
+    return showOrderSummary(paymentInfo);
+  })
+  .then(function (orderInfo) {
+    console.log(orderInfo);
+    return updateWalletBalance();
+  })
+  .then(function (updatedWalletBalance) {
+    console.log(updatedWalletBalance);
+  })
+  .catch(function (err) {
+    console.log(err.message);
+  });
+
+function createOrder(cart) {
+  const pr = new Promise(function (resolve, reject) {
+    if (!validateCart(cart)) {
+      const err = new Error("Cart is not valid");
+      reject(err);
+    }
+
+    const orderId = "12345";
+
+    if (orderId) {
+      setTimeout(() => {
+        resolve(orderId);
+      }, 5000);
+    }
+  });
+
+  return pr;
+}
+
+function validateCart(cart) {
+  return true;
+}
+
+function proceedToPayment(orderId) {
+  return new Promise(function (resolve, reject) {
+    resolve("Payment successful");
+  });
+}
+
+function showOrderSummary(paymentInfo) {
+  return new Promise(function (resolve, reject) {
+    resolve("Ordered shoe, pant and kurta 1 each respectively");
+  });
+}
+
+function updateWalletBalance() {
+  return new Promise(function (resolve, reject) {
+    resolve("Updated wallet balance: $1 million");
+  });
+}
+*/
